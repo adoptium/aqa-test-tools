cfd1971807ab4595c755baeccd519e21c043b2f0
lanxia <lan_xia@ca.ibm.com>
2018-08-23 14:49:29 -0400

    Add deleteUnusedOutput api
    Orphan outputs maybe created in output. deleteUnusedOutput is
    created to delete these orphan outputs. This API is for DB clean
    up purpose only.
    
    Signed-off-by: lanxia <lan_xia@ca.ibm.com>

--- TestResultSummaryService/routes/index.js
+++ TestResultSummaryService/routes/index.js
@@ -5,6 +5,7 @@ app.get( '/compareTests', wrap( require( "./compareTests" ) ) );
 app.get( '/deleteBuildListById', wrap( require( "./deleteBuildListById" ) ) );
 app.get( '/deleteBuildsAndChildrenByFields', wrap( require( "./deleteBuildsAndChildrenByFields" ) ) );
 app.get( '/deleteCollection', wrap( require( "./deleteCollection" ) ) );
+app.get( '/deleteUnusedOutput', wrap( require( "./deleteUnusedOutput" ) ) );
 app.get( '/getAllTestsWithHistory', wrap( require( "./getAllTestsWithHistory" ) ) );
 app.get( '/getBuildHistory', wrap( require( "./getBuildHistory" ) ) );
 app.get( '/getBuildList', wrap( require( "./getBuildList" ) ) );
