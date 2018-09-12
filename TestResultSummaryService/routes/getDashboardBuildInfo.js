066406f2ea2a720ecb53b143cdf519bc5763c104
lanxia <lan_xia@ca.ibm.com>
2018-09-12 14:20:38 -0400

    Read Dashboard build info from a json file
    Closes: #7
    
    Signed-off-by: lanxia <lan_xia@ca.ibm.com>

---
+++ TestResultSummaryService/routes/getDashboardBuildInfo.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+module.exports = async (req, res) => {
+    const content = fs.readFileSync(__dirname + "/../DashboardBuildInfo.json");
+    const jsonContent = JSON.parse(content);
+    res.send(jsonContent);
+}
