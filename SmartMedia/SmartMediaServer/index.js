4c14a585d5b6e2fc72823b856bfb6a400bbb3e99
Yuehan-Lin <Yuehan.Lin@ibm.com>
2019-04-12 16:26:35 -0400

    Added twitter api for Smart Media
    Fixed:#87
    Signed-off-by: Yuehan-Lin <Yuehan.Lin@ibm.com>

--- SmartMedia/SmartMediaServer/index.js
+++ SmartMedia/SmartMediaServer/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const ArgParser = require( './ArgParser' );
+ArgParser.parse();
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/getSearchTweets', require( "./getSearchTweets" ) );
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
