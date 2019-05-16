8b7459e419402a03223789de14dc26e692734319
Renfei Wang <renfeiw@ca.ibm.com>
2019-05-16 17:32:09 -0400

    Update topLevelBuilds page
    - udpated the logic behind Test Results and Build Results
    
    Signed-off-by: Renfei Wang <renfeiw@ca.ibm.com>

--- TestResultSummaryService/routes/getAllChildBuilds.js
+++ TestResultSummaryService/routes/getAllChildBuilds.js
@@ -2,10 +2,10 @@ const { TestResultsDB, ObjectID } = require('../Database');
 
 /*
 * This API takes parentId or combination of (url, buildName and buildNum)
-* Optional paramters: buildResult and buildNameRegex
+* Optional paramters: buildResult, testSummaryResult and buildNameRegex
 * It finds a parent build that has id = parentId or matches the combination of (url, buildName 
 * and buildNum)
-* It returns all descent builds of the parent build that matches with buildResult and buildNameRegex
+* It returns all descent builds of the parent build that matches with buildResult, testSummaryResult and buildNameRegex
 */
 
 module.exports = async (req, res) => {
@@ -14,6 +14,7 @@ module.exports = async (req, res) => {
   let buildNum = req.query.buildNum;
   let parentId = req.query.parentId;
   let buildResult = req.query.buildResult;
+  let testSummaryResult = req.query.testSummaryResult;
 
   let info = {};
   if (parentId) {
@@ -32,11 +33,16 @@ module.exports = async (req, res) => {
     buildResult = { $ne: buildResult.substring(1) };
   }
 
-  let matchData = { "childBuilds.buildResult": buildResult };
+  let matchData = {}
+  if (buildResult) {
+    matchData["childBuilds.buildResult"] = buildResult;
+  }
+  if (testSummaryResult) {
+    matchData[`childBuilds.testSummary.${testSummaryResult}`] = { $exists: true, $ne: null, $ne: 0 };
+  }
   if (req.query.buildNameRegex) {
     matchData["childBuilds.buildName"] = { $regex: req.query.buildNameRegex };
   }
-
   const testResultsDB = new TestResultsDB();
   const result = await testResultsDB.aggregate([
     { $match: info },
