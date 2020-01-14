billyshin/openjdk-test-tools

Add Parsers & Perf Graph for AcmeAir
- Based on ODM.jsx, added a perf graph for AcmeAir
- Added parsers for adding information to Node version

Issue #119
Issue #106

Signed-off-by: Jiayao Lin <jiayaolinn@gmail.com>

test-result-summary-client/src/Dashboard/Widgets/Graph/index.js
@@ -1,3 +1,4 @@
 export { default as DayTrader3 } from "./DayTrader3";
 export { default as ODM } from "./ODM";
 export { default as SPECjbb2015 } from "./SPECjbb2015";
+export { default as AcemairNodejs } from "./AcemairNodejs";

adoptium/aqa-test-tools

test-result-summary-client/src/Dashboard/Widgets/Graph/index.js
@@ -1,3 +1,4 @@
 export { default as DayTrader3 } from "./DayTrader3";
 export { default as ODM } from "./ODM";
 export { default as SPECjbb2015 } from "./SPECjbb2015";
+export { default as AcmeairNodejs} from "./AcmeairNodejs";