a90e99cb2fc88a88cca7cd151a7f427237e8e43b
lanxia <lan_xia@ca.ibm.com>
2020-07-14 14:54:55 -0400

    Fix warnings
    - upgrade 3rd party libs
    - fix warnings
    
    
    Signed-off-by: lanxia <lan_xia@ca.ibm.com>

--- test-result-summary-client/src/Dashboard/Widgets/Graph/utils.js
+++ test-result-summary-client/src/Dashboard/Widgets/Graph/utils.js
@@ -1,20 +1,18 @@
-module.exports = {
-    parseSha: function(str, sha) {
-        if (! str) {
-            return null;
-        }
-        str = str.split('\n');
-
-        for (let i of str) {
-            i = i.split('-');
-            if (i[0].trim() === sha) {
-                try {
-                    return i[1].trim();
-                } catch (e) {
-                    return null;
-                }
-            }
-        }
+export const parseSha = (str, sha) => {
+    if (!str) {
         return null;
     }
+    str = str.split('\n');
+
+    for (let i of str) {
+        i = i.split('-');
+        if (i[0].trim() === sha) {
+            try {
+                return i[1].trim();
+            } catch (e) {
+                return null;
+            }
+        }
+    }
+    return null;
 }
