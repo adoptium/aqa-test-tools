6dcda36d4b0e14862532e1f85e557daf1697be2b
OscarQQ <Yixin.Qian@ibm.com>
2021-02-20 23:53:23 -0800

    rename FVT to custom in Dashboard
    related: https://github.com/AdoptOpenJDK/openjdk-test-tools/issues/331
    Signed-off-by: OscarQQ <Yixin.Qian@ibm.com>

--- test-result-summary-client/src/Dashboard/Dashboard.jsx
+++ test-result-summary-client/src/Dashboard/Dashboard.jsx
@@ -7,8 +7,8 @@ export default class Dashboard extends Component {
 
     render() {
         return <Tabs type="card">
-            <TabPane tab="FVT" key="1"><TabInfo tab="FVT" /></TabPane>
-            <TabPane tab="Perf" key="2"><TabInfo tab="Perf" /></TabPane>
+            <TabPane tab="Perf" key="1"><TabInfo tab="Perf" /></TabPane>
+            <TabPane tab="Custom" key="2"><TabInfo tab="Custom" /></TabPane>
         </Tabs>
     }
 }
