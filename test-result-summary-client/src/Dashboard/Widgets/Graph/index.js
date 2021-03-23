46c2c544f2401f0f383aeda8566f4886a0e9bc01
yanshan24 <yanshan@ualberta.ca>
2021-03-23 17:35:49 +0800

    enable renaissance widget in dashboard
    Signed-off-by: yanshan24 <yanshan@ualberta.ca>

--- test-result-summary-client/src/Dashboard/Widgets/Graph/index.js
+++ test-result-summary-client/src/Dashboard/Widgets/Graph/index.js
@@ -3,3 +3,4 @@ export { default as ODM } from "./ODM";
 export { default as SPECjbb2015 } from "./SPECjbb2015";
 export { default as AcmeairNodejs} from "./AcmeairNodejs";
 export { default as Dacapo} from "./Dacapo";
+export { default as Renaissance} from "./Renaissance";
