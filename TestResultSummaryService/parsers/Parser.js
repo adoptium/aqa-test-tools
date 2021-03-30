2888af5427895cb4b0ce5478b7ae13ad09b96b50
Milder Hernandez <milderhc@microsoft.com>
2021-03-30 01:33:42 -0700

    Revert "Fix date from builds"
    This reverts commit f2b8a2193fb15f484a7210b8019198ae4a2d6fbe.

--- TestResultSummaryService/parsers/Parser.js
+++ TestResultSummaryService/parsers/Parser.js
@@ -9,12 +9,12 @@ class Parser {
 
     exactJavaVersion(output) {
         const javaVersionRegex = /=JAVA VERSION OUTPUT BEGIN=[\r\n]+([\s\S]*?)[\r\n]+.*=JAVA VERSION OUTPUT END=/;
-        const javaBuildDateRegex = /\[([0-9]{4})-?(0[1-9]|1[012])-?(0[1-9]|[12][0-9]|3[01])T/;
+        const javaBuildDateRegex = /\s([0-9]{4})-?(0[1-9]|1[012])-?(0[1-9]|[12][0-9]|3[01])/;
         const sdkResourceRegex = /.*?SDK_RESOURCE\=(.*)[\r\n]+/;
         let curRegexResult = null;
         let javaVersion, jdkDate, sdkResource;
         if ( ( curRegexResult = javaVersionRegex.exec( output ) ) !== null ) {
-            javaVersion = curRegexResult[1];
+            javaVersion = removeTimestamp(curRegexResult[1]);
         }
         curRegexResult = null;
         if ( ( curRegexResult = sdkResourceRegex.exec( output) ) != null) {
@@ -24,9 +24,7 @@ class Parser {
         // parse jdk date from javaVersion
         if ( ( curRegexResult = javaBuildDateRegex.exec( javaVersion ) ) !== null ) {
             jdkDate = curRegexResult[0];
-            jdkDate = " " + curRegexResult[1] + curRegexResult[2] + curRegexResult[3]
         }
-        javaVersion = removeTimestamp(javaVersion);
         return { javaVersion, jdkDate, sdkResource };
     }
 
