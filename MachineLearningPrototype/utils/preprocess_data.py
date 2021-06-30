5a5461afe68961c14ca18e11d1ec8bcab4b7bf2b
Avishree Khare <31511755+avishreekh@users.noreply.github.com>
2021-07-01 00:04:19 +0530

    Getting test info from TRSS servers for DeepAQAtik (#480)

--- MachineLearningPrototype/utils/preprocess_data.py
+++ MachineLearningPrototype/utils/preprocess_data.py
@@ -1,5 +1,6 @@
 import re
-
+import requests
+import json
 
 def remove_time_stamp(content):
     if not content:
@@ -15,3 +16,46 @@ def extract_quotation_content(content):
     if (len(substring) >= 1):
         result=' '.join(substring)
         return result
+
+def query_trss_for_jenkins_output(jenkins_url, test_name, trss_servers=["https://trss.adoptium.net"]):
+    #Get build info from TRSS
+    query_url = "https://trss.adoptium.net/api/parseJenkinsUrl"
+    query_params = {"jenkinsUrl": jenkins_url}
+    output = requests.get(query_url, query_params)
+    output_dict = json.loads(output.content)
+
+    if "output" not in output_dict:
+        raise Exception("No response received from TRSS while parsing Jenkins Url")
+        return ""
+    
+    output_dict = output_dict["output"]
+
+    if output_dict["errorMsg"]:
+        raise Exception("Failed to parse Jenkins Url with error message: " + output_dict["errorMsg"])
+        return ""
+
+    url = output_dict["serverUrl"]
+    build_name = output_dict["buildName"]
+    build_num = output_dict["buildNum"]
+
+    query_params = {"url": url,
+                    "buildName": build_name,
+                    "buildNum": build_num,
+                    "testName": test_name,}
+
+    print("Query parameters:")
+    print(query_params)
+
+    for server in trss_servers:
+        #Try to fetch test data from server
+        query_url = f"{server}/api/getOutputByTestInfo"
+        print(f"\nQuerying url: {query_url}")
+        
+        output = requests.get(query_url, query_params)
+        data = json.loads(output.content)
+
+        if "output" in data:
+            return data["output"]
+    
+    print("Couldn't find data for test")
+    return ""
