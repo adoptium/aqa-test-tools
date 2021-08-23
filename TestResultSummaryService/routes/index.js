8237c2121223cec0e6c7862170d88114c9500f21
Asjad Ahmed Khan <60435499+2001asjad@users.noreply.github.com>
2021-08-23 21:51:06 +0530

    Editted possibleIssues and added getFeedbackUrl file (#522)
    Co-authored-by: Lan Xia <Lan_Xia@ca.ibm.com>

--- TestResultSummaryService/routes/index.js
+++ TestResultSummaryService/routes/index.js
@@ -39,6 +39,7 @@ app.get( '/getTopLevelBuildNames', wrap( require( "./getTopLevelBuildNames" ) ) 
 app.get( '/getTotals', wrap( require( "./getTotals" ) ) );
 app.get( '/parseJenkinsUrl', wrap( require( "./parseJenkinsUrl" ) ) );
 app.get( '/populateDB', wrap( require( "./populateDB" ) ) );
+app.get( '/getFeedbackUrl', wrap( require( "./getFeedbackUrl" ) ) );
 
 app.get( '/updateComments', wrap( require( "./updateComments" ) ) );
 app.get( '/updateKeepForever', wrap( require( "./updateKeepForever" ) ) );
