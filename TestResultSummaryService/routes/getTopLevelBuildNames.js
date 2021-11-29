9edc974cb00c3161ec076b98b7d41b587361e6b7
shiyuxiu <xiushiyu666@gmail.com>
2021-11-29 11:23:54 -0700

    fix

--- TestResultSummaryService/routes/getTopLevelBuildNames.js
+++ TestResultSummaryService/routes/getTopLevelBuildNames.js
@@ -1,51 +1,31 @@
+const { query } = require('winston');
 const { TestResultsDB, ObjectID } = require('../Database');
 module.exports = async (req, res) => {
-    let { type } = req.query;
+    let { type, AQAvitCert} = req.query;
     const db = new TestResultsDB();
-    let result = ""
-    if ( req.query.AQAvitCert ){
-        result = await db.aggregate([
-            {
-                $match: {
-                    parentId: { $exists: false },
-                    type: type,
-                    AQAvitCert : true
-                }
-            },
-            {
-                $group: {
-                    _id: {
-                        url: '$url',
-                        buildName: '$buildName'
-                    },
-                }
-            },
-            {
-                $sort: { _id: 1 }
-            }
-        ]); 
-    }
-    else {
-        result = await db.aggregate([
-            {
-                $match: {
-                    parentId: { $exists: false },
-                    type: type
-                }
-            },
-            {
-                $group: {
-                    _id: {
-                        url: '$url',
-                        buildName: '$buildName'
-                    },
-                }
-            },
-            {
-                $sort: { _id: 1 }
-            }
-        ]);
-    }
     
+    let query = {}
+    query.parentId = { $exists: false }
+    query.type = type
+    if (AQAvitCert) {
+        query.AQAvitCert = AQAvitCert
+    }
+    let result = await db.aggregate([
+        {
+            $match: query
+        },
+        {
+            $group: {
+                _id: {
+                    url: '$url',
+                    buildName: '$buildName'
+                },
+            }
+        },
+        {
+            $sort: { _id: 1 }
+        }
+    ]);
     res.send(result);
+ 
 }
