62c9b196b260ea63aa370178aa3aabf42277992e
Afif Al Mamun <afifanik@gmail.com>
2023-04-25 22:25:51 +0600

    Add positive and negative counter for submitted feedback
    Signed-off-by: Afif Al Mamun <afifanik@gmail.com>

--- TestResultSummaryService/routes/postIssueFeedback.js
+++ TestResultSummaryService/routes/postIssueFeedback.js
@@ -49,44 +49,108 @@ async function writeTestOutputToFile(
         const fileName = `${testName}_${buildName}_${buildNum}_${domain}.txt`;
         const outputPath = `${__dirname}/../../MachineLearningPrototype/data/JenkinsDataWithFeedback/${fileName}`;
 
-        await fs.writeFile(outputPath, testOutput);
+        const fileExists = await fs.stat(outputPath).then(
+            () => true,
+            () => false
+        );
+
+        if (!fileExists) {
+            await fs.writeFile(outputPath, testOutput);
+        }
+
         return fileName;
     } catch (error) {
         throw error;
     }
 }
 
+async function getRecordIfExists(db, repoName, issueNumber, testId) {
+    const records = await db
+        .getData({ repoName, issueNumber, testId })
+        .toArray();
+
+    if (records.length == 0) {
+        return null;
+    }
+
+    return records[0];
+}
+
+async function insertNewFeedback(db, requestBody) {
+    const {
+        repoName,
+        issueName,
+        issueCreator,
+        testId,
+        testName,
+        buildName,
+        issueNumber,
+        accuracy,
+    } = requestBody;
+
+    const { testOutput, buildUrl, buildNum } = await getTestOutput(testId);
+    const outputFileName = await writeTestOutputToFile(
+        testName,
+        buildName,
+        buildUrl,
+        buildNum,
+        testOutput
+    );
+
+    const positiveCounter = Number(accuracy === 1);
+    const negativeCounter = Number(accuracy === 0);
+
+    await db.populateDB({
+        repoName,
+        issueName,
+        issueCreator,
+        issueNumber,
+        testName,
+        testId,
+        testOutput: outputFileName,
+        positiveCounter,
+        negativeCounter,
+    });
+}
+
+async function updateFeedback(db, record, accuracy) {
+    const options = { upsert: false };
+    const criteria = { _id: new ObjectID(record._id) };
+
+    let { positiveCounter, negativeCounter } = record;
+
+    if (accuracy === 0) {
+        negativeCounter += 1;
+    }
+
+    if (accuracy === 1) {
+        positiveCounter += 1;
+    }
+
+    await db.update(
+        criteria,
+        { $set: { positiveCounter, negativeCounter } },
+        options
+    );
+}
+
 module.exports = async (req, res) => {
     try {
         const db = new FeedbackDB();
-        const {
+        const { repoName, testId, issueNumber, accuracy } = req.body;
+
+        const record = await getRecordIfExists(
+            db,
             repoName,
-            issueName,
-            issueCreator,
-            testId,
-            testName,
-            buildName,
             issueNumber,
-            accuracy,
-        } = req.body;
-        const { testOutput, buildUrl, buildNum } = await getTestOutput(testId);
-        const outputFileName = await writeTestOutputToFile(
-            testName,
-            buildName,
-            buildUrl,
-            buildNum,
-            testOutput
+            testId
         );
-        await db.populateDB({
-            repoName,
-            issueName,
-            issueCreator,
-            issueNumber,
-            testName,
-            testId,
-            testOutput: outputFileName,
-            accuracy,
-        });
+
+        if (!record) {
+            await insertNewFeedback(db, req.body);
+        } else {
+            await updateFeedback(db, record, accuracy);
+        }
 
         res.send({ result: 'Feedback recorded' });
     } catch (error) {
