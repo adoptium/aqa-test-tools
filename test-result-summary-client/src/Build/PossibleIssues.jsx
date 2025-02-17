819019cfcd6c21696e9f38d250091a8b115687c3
denny-cjacob <57014313+dennycjacob@users.noreply.github.com>
2025-02-17 15:23:12 -0500

    Search adoptium/infrastructure and adoptium/aqa-build for Adoptium build only (#976)
    * Search for adoptium/infrastructure and adoptium/aqa-build for Adoptium build only
    
    Resolves: https://github.com/adoptium/aqa-test-tools/issues/962
    Signed-off-by :Denny Chacko Jacob <denny.cjacob@ibm.com>

--- test-result-summary-client/src/Build/PossibleIssues.jsx
+++ test-result-summary-client/src/Build/PossibleIssues.jsx
@@ -87,9 +87,12 @@ const PossibleIssues = () => {
         if (buildName.includes('j9') || buildName.includes('ibm')) {
             additionalRepo = '+repo:eclipse-openj9/openj9';
         }
+        else if (buildName.includes('hs')) {
+            additionalRepo = '+repo:adoptium/infrastructure+repo:adoptium/aqa-build';
+        }
         const response = await fetch(
             `https://api.github.com/search/issues?q=${generalTestName}+repo:adoptium/aqa-tests` +
-                `+repo:adoptium/infrastructure+repo:adoptium/aqa-build+repo:adoptium/aqa-systemtest+repo:adoptium/TKG${additionalRepo}`,
+                `+repo:adoptium/aqa-systemtest+repo:adoptium/TKG${additionalRepo}`,
             {
                 method: 'get',
             }
